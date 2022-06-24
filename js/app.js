6b25cfb32eca9796ec4db6d856f65d312d6b8fb5
Lauren Main <lauren.main28@gmail.com>
2022-06-23 20:43:19 -0700

    test save and load function and split up local storage

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 
 // Save to and load from Local Storage
 let allRecipes = [];
+//let allInstructions = [];
 
 //tbd if there should be separate arrays for parameters
 const Recipe = function (title, ingredients, instructions) {
@@ -12,8 +13,21 @@ const Recipe = function (title, ingredients, instructions) {
   allRecipes.push(this);
 };
 
+Recipe.prototype.saveEachToLocalStorage = function (){
+  let stringify1 = JSON.stringify (this.title); 
+  let stringify2 = JSON.stringify (this.ingredients); 
+  let stringify3 = JSON.stringify (this.instructions); 
+  localStorage.setItem("recipetitle", stringify1); 
+  localStorage.setItem("recipeingredients", stringify2); 
+  localStorage.setItem("recipeinstructions", stringify3); 
+
+}; 
+
+// let recipe1 = new Recipe("cake", "rake", "bake");
+// let recipe2 = new Recipe("cookie", "salmon", "pat");
+
 //save
-Recipe.prototype.saveToLocalStorage = function () {
+Recipe.prototype.saveAllToLocalStorage = function () {
   let stringify = JSON.stringify(allRecipes);
   localStorage.setItem("recipebook", stringify);
 };
@@ -21,8 +35,18 @@ Recipe.prototype.saveToLocalStorage = function () {
 //load
 Recipe.prototype.loadFromLocalStorage = function () {
   let loadRecipe = JSON.parse(localStorage.getItem("recipebook")) || [];
-  recipe = new Recipe(loadRecipe);
+   let recipe = new Recipe(loadRecipe);
+  allRecipes.push(recipe); 
 };
-let recipe1 = new Recipe("cake", "rake", "bake");
-console.log(allRecipes);
-allRecipes[0].saveToLocalStorage();
+
+//new recipe
+
+
+
+//console.log(allRecipes);
+// allRecipes[0].saveAllToLocalStorage();
+// for (let i = 0; i < 5; i++){
+//   let recipe = allRecipes[i]; 
+//   recipe.loadFromLocalStorage(); 
+//   console.log(recipe); 
+// }
