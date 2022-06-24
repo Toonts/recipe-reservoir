7e307317cdd8e97a44b7b0843147ad628f14a9b6
MARIO ACEVEDO <JUN_ACEVEDO415@YAHOO.COM>
2022-06-23 21:40:40 -0700

    added js

--- js/index.js
+++ js/index.js
@@ -4,3 +4,15 @@
 // Render list of searched recipes
 
 // Pull info from form into constructor function and save (put form in index.html)
+
+function render (){
+  let table = document.getElementById ("table");
+  for (let i=0; i < allRecipes.length; i++){
+    let recipe = allRecipes[i];
+    let tableRow = document.createElement ("tr");
+    let tableData = document.createElement ("td");
+    tableData.innerText = recipe.title;
+  
+  }
+
+}
