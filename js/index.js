4eb05ce72d0daeee2905e13637b4d29abd21f844
Daniel Brott <danbrott42@gmail.com>
2022-06-23 23:25:07 -0700

    added some listeners to the input fields

--- js/index.js
+++ js/index.js
@@ -5,6 +5,56 @@
 
 // Pull info from form into constructor function and save (put form in index.html)
 
+//add event listeners
+const input1 = document.getElementById('name');
+const input2 = document.getElementById('ingredients');
+const input3 = document.getElementById('rating');
+const input4 = document.getElementById('title');
+const input5 = document.getElementById('new-ingredients');
+const input6 = document.getElementById('instructions');
+input1.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    let nameValue = document.getElementById('name').value;
+    console.log(nameValue);
+  }
+});
+input2.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    let nameValue = document.getElementById('ingredients').value;
+    console.log(nameValue);
+  }
+});
+input3.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    let nameValue = document.getElementById('rating').value;
+    console.log(nameValue);
+  }
+});
+input4.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    let nameValue = document.getElementById('title').value;
+    console.log(nameValue);
+  }
+});
+input5.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    let nameValue = document.getElementById('new-ingredients').value;
+    console.log(nameValue);
+  }
+});
+input6.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    let nameValue = document.getElementById('instructions').value;
+    console.log(nameValue);
+  }
+});
+
 
 function render (){
   let table = document.getElementById ("table");
@@ -15,8 +65,6 @@ function render (){
     tableData.innerText = recipe.title;
     tableRow.appendChild(tableData);
     table.appendChild(tableRow);
-  
   }
-
 }
 
